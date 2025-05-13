d236080ac0c1cdc559800693e668ac74d4b11635
Thiago Vitor Lorena <thiago.vitor.2067@gmail.com>
2025-05-13 15:07:32 +0000

    Starting the project

---
+++ CalculadoraSimples.c
@@ -0,0 +1,23 @@
+int soma (int a, b) {
+    return a + b;
+}
+
+int subtracao (int a, b) {
+    return a - b;
+}
+
+int multiplicar (int a, b) {
+    return a * b;
+}
+
+int divisao (int a, b) {
+    if(b == 0){
+        printf("Não há divisão por 0")
+        return 0;
+    }   
+    return a / b;
+}
+
+int main(){
+    
+}
